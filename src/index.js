6c8d8b69051951b86dacd01346b2213119f14cb8
Vincent <vincecake008@gmail.com>
2024-10-31 09:12:33 -0300

    feat: index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import e from "express";
+import "dotenv/config.js";
+import "./db/server.js";
+import species_router from "./http:/routes/species-route.js";
+import explorer_router from "./http:/routes/explorer-route.js";
+import expedition_router from "./http:/routes/expedition-route.js";
 
+const app = e();
+
+app.use(e.json);
+app.use("/species", species_router);
+app.use("/explorer", explorer_router);
+app.use("/expedition", expedition_router);
+
+app.listen(process.env.API_PORT, () => console.log("Server running"));
